7321de42a0933d6060fa49718166ba7eb12a5008
afcolindres <arnold_fcolindres@yahoo.com>
2018-11-29 01:24:03 -0600

    funcionalidad registro usuarios y validaciones
    se registra los usuarios

--- Gafe_editor/index.js
+++ Gafe_editor/index.js
@@ -1,5 +1,51 @@
-var express = require("express"); //Importar el framework express
-var app = express(); //Inicializar la aplicacion con express
-app.use(express.static("public")); //Publicar un directorio de archivos estaticos
-app.listen(8008);//Levantar el servidor y escuchar en el puerto indicado
-console.log("Servidor levantado");
+var express = require("express");
+var mysql = require("mysql");
+var bodyParser = require("body-parser");
+var bcrypt = require('bcrypt-nodejs');
+var app = express();
+var credenciales = {
+    host:"localhost",
+    user:"root",
+    password:"",
+    port:"3306",
+    database: "bd_gafe"
+};
+
+app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+///****************Rutas de control************************* */
+app.post("/consultausuario", function(req,res){
+    var conexion = mysql.createConnection(credenciales);
+    conexion.query(`SELECT CODIGO_USUARIO, CORREO
+                    FROM tbl_usuarios 
+                    WHERE lower(correo)=lower(?)`,
+        [req.body.email],
+        function(error, data, fields){
+            console.log(error);
+            res.send(data);
+            res.end();
+            conexion.end();
+        }
+    );
+});
+
+///***************************registro de usuarios************************ */
+app.post("/registrarUsuario", function(req,res){
+    var conexion = mysql.createConnection(credenciales);
+    conexion.query(`INSERT INTO tbl_usuarios(NOMBRE, APELLIDOS, CORREO, CONTRASEÑA, CODIGO_GENERO) 
+                    VALUES (?,?,?,?,?)`,
+        [req.body.nombre,req.body.apellido,req.body.email, bcrypt.hashSync(req.body.password, null, null),req.body.genero],
+        function(error, data, fields){
+            console.log(error);
+            res.send(data);
+            res.end();
+            conexion.end();
+        }
+    );
+});
+
+app.listen(8008, function(){ 
+    console.log("Servidor iniciado");
+});
